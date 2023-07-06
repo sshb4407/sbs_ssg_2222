bc92657a016f827be05d04079b02d90233448c87
sshb4407 <sshb4407@naver.com>
2023-07-06 15:26:45 +0900

    게시물상세페이지기능구현,날짜는 아직못함

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -37,17 +37,42 @@ public class Main {
 
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} else if (command.equals("article list")) {
-				if ( articles.size() == 0 ) {
+				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}
-				
+
 				System.out.println("번호 | 제목");
-				for ( int i = articles.size() - 1; i >= 0; i-- ) {
+				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
-					
-					System.out.printf("%d, %s\n", article.id, article.title);
+
+					System.out.printf("%d    |  %s\n", article.id, article.title);
 				}
+			} else if (command.startsWith("article detail")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if ( foundArticle == null ) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", "2023-12-12");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+				
 			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
