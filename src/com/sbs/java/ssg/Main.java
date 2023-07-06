1c0287c652196d208c0a6fc197656b1a5ce8ed68
sshb4407 <sshb4407@naver.com>
2023-07-06 13:28:02 +0900

    system exit, article list, trim() 작성

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -5,14 +5,28 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 ==");
-
 		Scanner sc = new Scanner(System.in);
-		System.out.printf("명령어) ");
-		String command;
-		command = sc.nextLine(); 
-		System.out.printf("입력된 명령어 : %s\n", command);
-
-
+		
+		while ( true ) {
+			System.out.printf("명령어) ");
+			String command = sc.nextLine();
+			command = command.trim();
+			
+			if ( command.length() == 0 ) {
+				continue;
+			}
+			
+			if ( command.equals("system exit") ) {
+				break;
+			}
+			else if ( command.equals("article list") ) {
+				System.out.println("게시물이 없습니다.");
+			}
+			else {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+			}
+		}
+		
 		sc.close();	
 		System.out.println("== 프로그램 끝 ==");
 	}
