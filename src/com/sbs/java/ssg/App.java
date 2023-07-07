bd939a8024fa7d14d93638a245a181ea084feb1c
sshb4407 <sshb4407@naver.com>
2023-07-07 12:28:07 +0900

    구조개선,MemberController 도입

--- src/com/sbs/java/ssg/App.java
+++ src/com/sbs/java/ssg/App.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.sbs.java.ssg.controller.ArticleController;
+import com.sbs.java.ssg.controller.MemberController;
 import com.sbs.java.ssg.dto.Article;
 import com.sbs.java.ssg.dto.Member;
 import com.sbs.java.ssg.util.Util;
@@ -24,6 +26,9 @@ public class App {
 		makeTastData();
 
 		Scanner sc = new Scanner(System.in);
+		
+		MemberController memberController = new MemberController(sc, members);
+		ArticleController articleController = new ArticleController();
 
 		while (true) {
 			System.out.printf("명령어) ");
@@ -36,48 +41,8 @@ public class App {
 
 			if (command.equals("system exit")) {
 				break;
-			}else if (command.equals("member join")) {
-				int id = members.size() + 1;
-				String regDate = Util.getNowDateStr();
-				
-				String loginId = null;
-				
-				while ( true ) {
-					System.out.printf("로그인 아이디 : ");
-					loginId = sc.nextLine();
-					
-					if ( isJoinableLoginId(loginId) == false ) {
-						System.out.printf("%s(은)는 이미 사용중인 아이디입니다.\n", loginId);
-						continue;
-					}
-					
-					break;
-				}
-				
-				String loginPw = null;
-				String loginPwConfirm = null;
-				
-				while ( true ) {
-					System.out.printf("로그인 비번 : ");
-					loginPw = sc.nextLine();
-					System.out.printf("로그인 비번확인 : ");
-					loginPwConfirm = sc.nextLine();
-					
-					if ( loginPw.equals(loginPwConfirm) == false ) {
-						System.out.println("비밀번호를 다시 입력해주세요.");
-						continue;
-					}
-					
-					break;
-				}
-				
-				System.out.printf("이름 : ");
-				String name = sc.nextLine();
-
-				Member member = new Member(id, regDate, loginId, loginPw, name);
-				members.add(member);
-
-				System.out.printf("%d번 회원이 생성되었습니다. 환영합니다^^\n", id);
+			} else if (command.equals("member join")) {
+				memberController.doJoin();
 			} else if (command.equals("article write")) {
 				int id = articles.size() + 1;
 				String regDate = Util.getNowDateStr();
@@ -184,29 +149,7 @@ public class App {
 		System.out.println("== 프로그램 끝 ==");
 	}
 	
-	private int getMemberIndexByLoginId(String loginId) {
-		int i = 0;
-		
-		for ( Member member : members ) {
-			if ( member.loginId.equals(loginId) ) {
-				return i;
-			}
-			
-			i++;
-		}
-		
-		return -1;
-	}
-
-	private boolean isJoinableLoginId(String loginId) {
-		int index = getMemberIndexByLoginId(loginId);
-		
-		if ( index == -1 ) {
-			return true;
-		}
-		
-		return false;
-	}
+	
 
 	private int getArticleIndexById(int id) {
 		int i = 0;
---
+++ src/com/sbs/java/ssg/controller/ArticleController.java
@@ -0,0 +1,5 @@
+package com.sbs.java.ssg.controller;
+
+public class ArticleController {
+
+}
