90aac7ab79570bd89f64bea37f971937830ef17f
sshb4407 <sshb4407@naver.com>
2023-07-10 10:26:13 +0900

    로그아웃 기능구현,로그인,로그아웃액션실행전에 로그인상태체크

--- src/com/sbs/java/ssg/controller/MemberController.java
+++ src/com/sbs/java/ssg/controller/MemberController.java
@@ -32,12 +32,15 @@ public class MemberController extends Controller {
 		case "login":
 			doLogin();
 			break;
+		case "logout":
+			doLogout();
+			break;
 		default:
 			System.out.println("존재하지 않는 명령어 입니다.");
 			break;
 		}
 	}
-	
+
 	public void makeTestData() {
 		System.out.println("테스트를 위한 데이터를 생성합니다.");
 		
@@ -125,6 +128,11 @@ public class MemberController extends Controller {
 	}
 	
 	private void doLogin() {
+		if ( isLogined() ) {
+			System.out.println("이미 로그인이 되어 있습니다.");
+			return;
+		}
+		
 		System.out.printf("로그인 아이디 : ");
 		String loginId = sc.nextLine();
 		System.out.printf("로그인 비번 : ");
@@ -147,7 +155,20 @@ public class MemberController extends Controller {
 		
 		System.out.printf("로그인 성공! %s님 환영합니다!\n", loginedMember.name);
 	}
-
 	
+	private boolean isLogined() {
+		return loginedMember != null;
+	}
+
+	private void doLogout() {
+		if ( isLogined() == false ) {
+			System.out.println("로그인 상태가 아닙니다.");
+			return;
+		}
+		
+		loginedMember = null;
+		
+		System.out.println("로그아웃 되었습니다.");
+	}
 
 }
