d3fbcfe90574ba71331ccd86cc5fdd168e20f855
sshb4407 <sshb4407@naver.com>
2023-07-11 10:07:54 +0900

    게시물 리스트에서 작성자 이름이 나오도록 수정

--- src/com/sbs/java/ssg/controller/ArticleController.java
+++ src/com/sbs/java/ssg/controller/ArticleController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.sbs.java.ssg.container.Container;
 import com.sbs.java.ssg.dto.Article;
 import com.sbs.java.ssg.dto.Member;
 import com.sbs.java.ssg.util.Util;
@@ -17,7 +18,8 @@ public class ArticleController extends Controller {
 
 	public ArticleController(Scanner sc) {
 		this.sc = sc;
-		this.articles = new ArrayList<Article>();
+		
+		articles = Container.articleDao.articles;
 	}
 
 	public void doAction(String command, String actionMethodName) {
@@ -117,11 +119,21 @@ public class ArticleController extends Controller {
 			}
 		}
 
-		System.out.println("번호 | 작성자 | 조회 | 제목");
+		System.out.println("번호 |   작성자  | 조회 | 제목");
 		for (int i = forListArticles.size() - 1; i >= 0; i--) {
 			Article article = forListArticles.get(i);
+			String writerName = null;
+			
+			List<Member> members = Container.memberDao.members;
+			
+			for ( Member member : members ) {
+				if ( article.memberId == member.id ) {
+					writerName = member.name;
+					break;
+				}
+			}
 
-			System.out.printf("%4d | %6d | %4d | %s\n", article.id, article.memberId, article.hit, article.title);
+			System.out.printf("%4d | %6s | %4d | %s\n", article.id, writerName, article.hit, article.title);
 		}
 	}
 
